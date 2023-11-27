f8e59babed9563edaa5a3fc5393fb733a4ec37d8
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-11-27 20:01:23 +0530

    project23

---
+++ Project 23 - 3D Card/script.js
@@ -0,0 +1,34 @@
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+const header = document.querySelector(".header");
+const shoe = document.querySelector(".shoe img");
+const addToCart = document.querySelector(".addToCart");
+const description = document.querySelector(".content h3");
+const shoeSizes = document.querySelector(".shoeSizes");
+
+console.log(card, container, header, shoe, addToCart, description, shoeSizes);
+
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  header.style.transform = "translateZ(150px)";
+  shoe.style.transform = "translateZ(200px) rotateZ(-45deg)";
+  description.style.transform = "translateZ(125px)";
+  shoeSizes.style.transform = "translateZ(100px)";
+  addToCart.style.transform = "translateZ(75px)";
+});
+
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transition = `rotateY(0deg) rotateX(0deg)`;
+  header.style.transform = "translateZ(0px)";
+  shoe.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  shoeSizes.style.transform = "translateZ(0px)";
+  addToCart.style.transform = "translateZ(0px)";
+});
