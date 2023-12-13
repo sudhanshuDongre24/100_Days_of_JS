656b8a27e33ee8960c356a0865b588e5db2b3492
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-12-13 09:30:17 +0530

    project49

---
+++ Project 49 - Scrolling Images/script.js
@@ -0,0 +1,20 @@
+const card = document.querySelectorAll(".card");
+window.addEventListener("scroll", animateCards);
+
+animateCards();
+
+function animateCards() {
+  const checkingBottom = (window.innerHeight / 4) * (125 / 25);
+
+  card.forEach((singleCard) => {
+    const cardTop = singleCard.getBoundingClientRect().top;
+
+    let toSlideIn = cardTop < checkingBottom;
+
+    if (toSlideIn) {
+      singleCard.classList.add("slidingIn");
+    } else {
+      singleCard.classList.remove("slidingIn");
+    }
+  });
+}
