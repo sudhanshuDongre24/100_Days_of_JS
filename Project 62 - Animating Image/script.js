630ce765b9c7474722004b1331c742419870340b
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-12-22 01:22:08 +0530

    project 62

---
+++ Project 62 - Animating Image/script.js
@@ -0,0 +1,43 @@
+const container = document.getElementById("container");
+const smallParts = 50;
+const parts = [];
+
+const widthOfPart = Math.ceil(window.innerWidth / smallParts);
+const heightOfPart = Math.ceil(window.innerHeight / smallParts);
+
+createBlocks();
+
+function createBlocks() {
+  for (let i = 0; i < smallParts; i++) {
+    const row = [];
+
+    for (let j = 0; j < smallParts; j++) {
+      const block = document.createElement("div");
+      block.classList.add("block");
+      block.style.width = widthOfPart + "px";
+      block.style.height = heightOfPart + "px";
+      block.style.top = "100vh";
+      block.style.left = "100vw";
+      block.style.backgroundPosition = `${-widthOfPart * j}px ${
+        -heightOfPart * i
+      }px`;
+      row.push(block);
+      container.appendChild(block);
+    }
+    parts.push(row);
+  }
+
+  setTimeout(() => {
+    animateBlocks();
+  }, 1000);
+}
+
+function animateBlocks() {
+  parts.forEach((row, i) => {
+    row.forEach((block, j) => {
+      block.style.top = heightOfPart * i + "px";
+      block.style.left = widthOfPart * j + "px";
+      block.style.transitionDelay = i * smallParts + j * smallParts + "ms";
+    });
+  });
+}
