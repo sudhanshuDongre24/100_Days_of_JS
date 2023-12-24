c1d1078d9e42fac210c11d12e476fcec16821502
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2023-12-24 10:10:15 +0530

    project65

---
+++ Project 65 - Checklist/script.js
@@ -0,0 +1,26 @@
+const checkboxes = document.querySelectorAll("[type=checkbox]");
+
+let lastCheck = -1;
+
+for (let i = 0; i < checkboxes.length; i++) {
+  whenClicked(checkboxes[i], i);
+}
+
+function whenClicked(box, idx) {
+  box.addEventListener("click", function () {
+    if (lastCheck >= 0) {
+      if (event.shiftKey) {
+        if (idx > lastCheck) {
+          for (let i = idx; i >= lastCheck; i--) {
+            checkboxes[i].checked = true;
+          }
+        } else {
+          for (let i = idx; i <= lastCheck; i++) {
+            checkboxes[i].checked = true;
+          }
+        }
+      }
+    }
+    lastCheck = idx;
+  });
+}
