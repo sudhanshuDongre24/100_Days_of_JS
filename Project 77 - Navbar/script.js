ee7083d1396c82f9540145d0fe249207d6c98a60
sudhanshuDongre24 <sudhanshuDongre24@gmail.com>
2024-01-04 08:57:48 +0530

    project 77

---
+++ Project 77 - Navbar/script.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector(".toggleBtn");
+const links = document.querySelector(".links");
+
+toggleBtn.addEventListener("click", () => {
+  if (links.classList.contains("active")) {
+    links.classList.remove("active");
+  } else {
+    links.classList.add("active");
+  }
+});
